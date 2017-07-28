fd5c8aa5ab44df3fd3bd58c04404960e31f36544
Surya <suryak0512@gmail.com>
2017-07-28 08:08:10 -0400

    Updated test cases

--- src/com/way2Automation/test/TestLogin.java
+++ src/com/way2Automation/test/TestLogin.java
@@ -19,8 +19,8 @@ public class TestLogin extends AbstractTest{
 	}
 	
 	@Test
-	@Parameters({TestConstants.FIELD_EMAIL , TestConstants.FIELD_PASSWORD})
-	public void testLogin(String username, String password) throws InterruptedException{
+	@Parameters({TestConstants.FIELD_EMAIL , TestConstants.FIELD_PASSWORD, TestConstants.FIELD_TESTCASE_ID})
+	public void testLogin(String username, String password, String tc_id) throws InterruptedException{
 		// Find signin link button
 		By signIn = By.className(TestConstants.FIELD_SIGNIN);
 		testDriver.findElement(signIn).click();
@@ -35,8 +35,27 @@ public class TestLogin extends AbstractTest{
 		testDriver.findElement(loginButton).click();
 
 		// Find alert element
-		WebElement loggedInUser = testDriver.findElement(By.className(TestConstants.FIELD_ACCOUNT));
-		Assert.assertEquals(loggedInUser.getText(),TestConstants.FIELD_ACCOUNT);
+		//Valid username and password
+		if(tc_id == "1"){
+			WebElement loggedInUser = testDriver.findElement(By.className(TestConstants.FIELD_ACCOUNT));
+			Assert.assertEquals(loggedInUser.getText(),TestConstants.FIELD_USER_NAME);
+		}
+		//IV U + V P  , IV U+ IV P, IV U + No P
+		if(tc_id == "2"){
+			testDriver.getPageSource().contains(TestConstants.ALERT_TEXT2);	
+		}
+		//V U + No P
+		if(tc_id == "3"){
+			testDriver.getPageSource().contains(TestConstants.ALERT_TEXT1);
+		}
+		//No U + IV P, No U+ V P
+		if(tc_id == "4"){
+			testDriver.getPageSource().contains(TestConstants.ALERT_TEXT);		
+			}
+		// V U + IV P
+		if(tc_id == "5"){
+			testDriver.getPageSource().contains(TestConstants.ALERT_TEXT3);		
+			}
 	}
 	
 	@AfterTest
